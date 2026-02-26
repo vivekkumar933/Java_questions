4f09d1b6cc0aaf560f0dcb8dd0480cbf7d727984
vivekkumar933 <vivekkumar38115@gmail.com>
2026-02-26 18:43:04 +0530

    initial commit

---
+++ src/advanced_patterns/HalfPyramid.java
@@ -0,0 +1,12 @@
+package advanced_patterns;
+
+public class HalfPyramid {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
