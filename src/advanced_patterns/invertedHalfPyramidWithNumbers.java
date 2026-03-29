584a6691b6ab89d1440b4b4e5a98de70d4a48f83
vivekkumar933 <vivekkumar38115@gmail.com>
2026-03-29 17:40:03 +0530

    inverted half pyramid with numbers

---
+++ src/advanced_patterns/invertedHalfPyramidWithNumbers.java
@@ -0,0 +1,18 @@
+package advanced_patterns;
+
+public class invertedHalfPyramidWithNumbers{
+    public static void invertedHalfPyramid(int n){
+        for (int i=1;i<=n;i++){
+        for(int j=1;j<=n-i+1;j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+    }
+    
+    
+
+    public static void main (String args[]){
+        invertedHalfPyramid(5);
+    }
+}
