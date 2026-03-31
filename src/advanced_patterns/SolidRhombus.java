d621e0efaee363e440bd5591bf761b47a24c47c2
vivekkumar933 <vivekkumar38115@gmail.com>
2026-03-31 13:31:49 +0530

    Solid Rhombus Pattern

---
+++ src/advanced_patterns/SolidRhombus.java
@@ -0,0 +1,32 @@
+/*
+      *******
+     *******
+    *******
+   *******
+  *******
+ *******
+*******
+*/
+
+package advanced_patterns;
+
+public class SolidRhombus {
+    public static void solidRhombusPattern(int n){
+        // Outer loop
+        for(int i=1;i<=n;i++){
+            //inner loop
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static void main(String[] args) {
+        solidRhombusPattern(7);
+    }
+}
