16a300b1d87c72d1cdfb5e4df89ceecfc38cd189
vivekkumar933 <vivekkumar38115@gmail.com>
2026-04-09 12:15:09 +0530

    Find shortest path

--- src/String/shortestPath.java
+++ src/String/shortestPath.java
@@ -22,18 +22,13 @@ public class shortestPath {
                else{
                 x++;
                }
-        }   
-        
+        }    
         int X2=x*x;
         int Y2=y*y;
         return (float)Math.sqrt(X2+Y2);
-
      }
-
-
      public static void main(String[] args) {
         String path="WNEENESENNN";
         System.out.println(getShortestPath(path));
-     }
-    
+     } 
 }    
