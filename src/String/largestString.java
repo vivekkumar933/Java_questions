fba363e9363f7eb73750a2415fdd02ee5b4fd720
vivekkumar933 <vivekkumar38115@gmail.com>
2026-04-11 12:17:11 +0530

    alrgest string in Lexicographic order

---
+++ src/String/largestString.java
@@ -0,0 +1,16 @@
+// For a given set of strings ,print thr largest string (In Lexicogrsphic order)
+package String;
+
+public class largestString {
+    public static void main(String[] args) {
+        String fruits[] = {"apple","mango","banana"};
+        String largest=fruits[0];
+      
+        for(int i=1;i<fruits.length;i++){
+            if(largest.compareTo(fruits[i])<0){
+                largest=fruits[i];
+            }
+        }
+        System.out.println(largest);
+}
+}
